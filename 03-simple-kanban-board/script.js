9fd64d6c08877d94076b9d2e8d4e2d5075974bd2
ailearningguy <ailearningguy@gmail.com>
2025-09-30 10:28:43 +0700

    Bat dau tao event dragHanler

--- 03-simple-kanban-board/script.js
+++ 03-simple-kanban-board/script.js
@@ -1,3 +1,30 @@
 const toDoBox = document.getElementById("to-do");
 const inProgressBox = document.getElementById("in-progress");
 const doneBox = document.getElementById("done");
+
+console.log(typeof toDoBox, toDoBox);
+console.log(inProgressBox);
+console.log(doneBox);
+
+const itemsToDo = [...toDoBox.getElementsByClassName("item")];
+
+itemsToDo.forEach((element) => {
+  element.addEventListener("dragstart", dragHandler);
+});
+
+const itemsInProgress = [...inProgressBox.getElementsByClassName("item")];
+
+itemsInProgress.forEach((element) => {
+  element.addEventListener("dragstart", dragHandler);
+});
+
+const itemsDone = [...doneBox.getElementsByClassName("item")];
+
+itemsDone.forEach((element) => {
+  element.addEventListener("dragstart", dragHandler);
+});
+
+function dragHandler(ev) {
+  const etarget = ev.target;
+  console.log(etarget);
+}
