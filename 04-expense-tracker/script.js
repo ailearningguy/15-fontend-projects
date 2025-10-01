333f7d564aaed7b3e397c5b62d2ce4b58c273caf
ailearningguy <ailearningguy@gmail.com>
2025-10-01 15:27:02 +0700

    Hoan thanh feature tinh balance

--- 04-expense-tracker/script.js
+++ 04-expense-tracker/script.js
@@ -1,6 +1,7 @@
 const balanceNumber = document.getElementById("balance-number");
 const incomeNumber = document.getElementById("income-number");
 const expenseNumber = document.getElementById("expense-number");
+const amountNumber = document.querySelector('input[name="amount"]');
 
 const listTransactions = document.getElementById("list-transactions");
 const transactionForm = document.getElementById("transaction-form");
@@ -9,9 +10,23 @@ const addBtn = document.getElementById("add-btn");
 const stored = JSON.parse(localStorage.getItem("transactions") || "[]");
 const transactionsData = stored;
 
-let income = Number(localStorage.getItem("income"));
-let expenses = Number(localStorage.getItem("expenses"));
-let balance = income - expenses;
+function balanceCal() {
+  let income = 0;
+  let expenses = 0;
+  let balance = 0;
+  transactionsData.forEach((item) => {
+    if (item.type === "income") {
+      income += Number(item.amount);
+    } else {
+      expenses += Number(item.amount);
+    }
+    balance = income - expenses;
+    incomeNumber.textContent = income.toLocaleString("vi-VN");
+    expenseNumber.textContent = expenses.toLocaleString("vi-VN");
+    balanceNumber.textContent = balance.toLocaleString("vi-VN");
+  });
+  console.log(balance);
+}
 
 function addButtonHandler(ev) {
   let typeOfTrans = document.querySelector(
@@ -21,6 +36,7 @@ function addButtonHandler(ev) {
     'input[name="description"]'
   )?.value;
   let amountInput = document.querySelector('input[name="amount"]')?.value;
+  console.log(amountInput);
   if (!typeOfTrans || !descriptionInput || !amountInput) {
     alert(`Bạn cần nhập đầy đủ thông tin:
     - Description
@@ -37,7 +53,7 @@ function addButtonHandler(ev) {
       transID: transRandID(),
       type: typeOfTrans,
       description: descriptionInput,
-      amount: Number(amountInput),
+      amount: Number(amountInput.replace(/[^\d]/g, "")),
     };
     transactionsData.push(dataItem);
     localStorage.setItem("transactions", JSON.stringify(transactionsData));
@@ -45,14 +61,19 @@ function addButtonHandler(ev) {
     const list = document.createElement("li");
     list.classList.add("item", dataItem.type);
     list.setAttribute("data-trans-id", dataItem.transID);
-    list.innerHTML = `<span class="transacion-name">${dataItem.description}</span
+    list.innerHTML = `<span class="transaction-name">${
+      dataItem.description
+    }</span
             ><span class="item-info-end"
-              ><span class="transaction-amount">${dataItem.amount}</span
+              ><span class="transaction-amount">${dataItem.amount.toLocaleString(
+                "vi-VN"
+              )}</span
               ><span class="delete-btn"></span
             ></span>`;
-    listTransactions.insertBefore(list, listTransactions.firstChild);
+    listTransactions.insertBefore(list, listTransactions.firstChild); // Chèn list mới vào đầu danh sách
 
     transactionForm.reset();
+    balanceCal(); // Reset form khi nhấn nút Add Transaction
   }
 }
 
@@ -64,14 +85,26 @@ function transactionRender() {
       const list = document.createElement("li");
       list.classList.add("item", item.type);
       list.setAttribute("data-trans-id", item.transID);
-      list.innerHTML = `<span class="transacion-name">${item.description}</span
+      list.innerHTML = `<span class="transaction-name">${item.description}</span
             ><span class="item-info-end"
-              ><span class="transaction-amount">${item.amount}</span
+              ><span class="transaction-amount">${item.amount.toLocaleString(
+                "vi-VN"
+              )}</span
               ><span class="delete-btn"></span
             ></span>`;
       listTransactions.appendChild(list);
     });
 }
 
+function inputFormat(e) {
+  const raw = e.target.value;
+  const digitsOnly = Number(raw.replace(/[^\d]/g, ""));
+  const formatted = digitsOnly.toLocaleString("vi-VN");
+  console.log(formatted, typeof formatted);
+  e.target.value = formatted;
+}
+
+amountNumber.addEventListener("input", inputFormat);
 addBtn.addEventListener("click", addButtonHandler);
 transactionRender();
+balanceCal();
